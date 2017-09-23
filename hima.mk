f0a0db4930218bc34b5bbbef99f270fb7130fcb7
me2151 <dillonbrosche@gmail.com>
2017-09-23 14:05:32 -0400

    Add HIDL Display Hal

--- hima.mk
+++ hima.mk
@@ -103,12 +103,20 @@ PRODUCT_PACKAGES += \
     memtrack.msm8994 \
     liboverlay
 
+PRODUCT_PACKAGES += \
+		android.hardware.graphics.composer@2.1-impl \
+		android.hardware.graphics.allocator@2.0-impl \
+		android.hardware.graphics.allocator@2.0-service \
+		android.hardware.graphics.mapper@2.0-impl \
+		android.hardware.memtrack@1.0-impl \
+		android.hardware.renderscript@1.0-impl
+
 # Dot View Case
 PRODUCT_PACKAGES += \
     Dotcase
 
 # Gesture Handler
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     GestureHandler
 
 # GPS
